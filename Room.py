785850679fbc687d26a637940adeb4fe019365f0
Zhongbob <89343697+Zhongbob@users.noreply.github.com>
2025-07-30 22:19:47 +0800

    added multiplayer

--- Room.py
+++ Room.py
@@ -3,6 +3,7 @@ import json
 from utils import calculate_median, angle_difference
 from fastapi.websockets import WebSocketDisconnect
 import math 
+from uuid import uuid4
 next_calibration = {
     "center": "left",
     "left": "right",
@@ -14,26 +15,42 @@ class Room:
         self.computer_client: ComputerClient = None
         self.mobile_clients: list[MobileClient] = []
         self.calibrations: dict[str, dict[str, list[float]]] = {
-            "x": {},
-            "y": {}
+
         }
         self.calibrations_final: dict[str, dict[str, float]] = {
-            "x": {},
-            "y": {}
-        }
-        self.current_calibration_position = "center"
 
-    def set_computer_client(self, websocket):
+        }
+        self.current_calibration_position = {
+
+        }
+
+    async def set_computer_client(self, websocket):
+        uuid = str(uuid4())
         if (self.computer_client is not None):
             print("Replacing existing computer client.")
             self.remove_computer_client()
-        new_computer = ComputerClient(websocket)
+        new_computer = ComputerClient(websocket, uuid, self)
         self.computer_client = new_computer
+        for mobile in self.mobile_clients:
+            await new_computer.new_player_connected(mobile.id)
         return new_computer
 
-    def add_mobile_client(self, websocket):
-        new_mobile = MobileClient(websocket, self)
+    async def add_mobile_client(self, websocket):
+        uuid = str(uuid4())
+        new_mobile = MobileClient(websocket, uuid, self)
+        self.current_calibration_position[uuid] = "center"
+        self.calibrations[uuid] = {
+            "x": {},
+            "y": {}
+        }
+        self.calibrations_final[uuid] = {
+            "x": {},
+            "y": {}
+        }
         self.mobile_clients.append(new_mobile)
+        if self.computer_client:
+            print(f"New mobile client connected: {uuid}")
+            await self.computer_client.new_player_connected(uuid)
         return new_mobile
     
     def remove_client(self, client):
@@ -57,48 +74,59 @@ class Room:
             "mobile": self.mobile_clients
         }
         
-    def clear_calibrations(self):
-        self.calibrations['x'].clear()
-        self.calibrations['y'].clear()
-        self.calibrations_final['x'].clear()
-        self.calibrations_final['y'].clear()
+    def clear_calibrations(self, player_id=None):
+        if player_id:
+            if player_id in self.calibrations:
+                del self.calibrations[player_id]
+            if player_id in self.calibrations_final:
+                del self.calibrations_final[player_id]
+            if player_id in self.current_calibration_position:
+                del self.current_calibration_position[player_id]
+        else:
+            self.calibrations.clear()
+            self.calibrations_final.clear()
+            self.current_calibration_position.clear()
     
-    async def on_calibration_complete(self):
+    async def on_calibration_complete(self, player_id):
         print("Calibration complete. Final calibrations:", self.calibrations)
-        self.calibrations_final = {
-            "x": {k: calculate_median(v) for k, v in self.calibrations['x'].items()},
-            "y": {k: calculate_median(v) for k, v in self.calibrations['y'].items()}
+        self.calibrations_final[player_id] = {
+            "x": {k: calculate_median(v) for k, v in self.calibrations[player_id]['x'].items()},
+            "y": {k: calculate_median(v) for k, v in self.calibrations[player_id]['y'].items()}
         }
-        self.calibrations_final['x']["range"] = angle_difference(self.calibrations_final['x']["left"], self.calibrations_final['x']["right"])
-        self.calibrations_final['y']["range"] = angle_difference(self.calibrations_final['y']["left"], self.calibrations_final['y']["right"])
+        self.calibrations_final[player_id]['x']["range"] = angle_difference(self.calibrations_final[player_id]['x']["left"], self.calibrations_final[player_id]['x']["right"])
+        self.calibrations_final[player_id]['y']["range"] = angle_difference(self.calibrations_final[player_id]['y']["left"], self.calibrations_final[player_id]['y']["right"])
         print("Final calibrations:", self.calibrations_final)
         if self.computer_client:
-            await self.computer_client.on_calibration_complete()
+            await self.computer_client.on_calibration_complete(player_id)
         for mobile in self.mobile_clients:
-            await mobile.on_calibration_complete()
+            if mobile.id == player_id:
+                await mobile.on_calibration_complete()
 
-    async def next_calibration_position(self, position):
-        self.current_calibration_position = next_calibration.get(position, None)
+    async def next_calibration_position(self, position, player_id):
+        print(f"Next calibration position for {player_id}: {position}")
+        self.current_calibration_position[player_id] = next_calibration.get(position, None)
         print(f"Next calibration position: {self.current_calibration_position}")
-        if self.current_calibration_position is None:
+        if self.current_calibration_position[player_id] is None:
             print("Calibration complete.")
-            await self.on_calibration_complete()
+            await self.on_calibration_complete(player_id)
             return None
-        await self.computer_client.request_calibration(self.current_calibration_position)
+        await self.computer_client.request_calibration(self.current_calibration_position[player_id], player_id)
         for mobile in self.mobile_clients:
-            await mobile.request_calibration(self.current_calibration_position)
-        return self.current_calibration_position
-    
-    async def add_calibration_data(self, position, x, y):
-        if position not in self.calibrations['x']:
-            self.calibrations['x'][position] = []
-        if position not in self.calibrations['y']:
-            self.calibrations['y'][position] = []
-        self.calibrations['x'][position].append(x)
-        self.calibrations['y'][position].append(y)
-        print(f"Calibration for {position}: {self.calibrations['x'][position]}")
-        if len(self.calibrations['x'][position]) >= 5 and len(self.calibrations['y'][position]) >= 5:
-            await self.next_calibration_position(position)
+            if mobile.id == player_id:
+                await mobile.request_calibration(self.current_calibration_position[player_id])
+        return self.current_calibration_position[player_id]
+
+    async def add_calibration_data(self, position, x, y, player_id):
+        if position not in self.calibrations[player_id]['x']:
+            self.calibrations[player_id]['x'][position] = []
+        if position not in self.calibrations[player_id]['y']:
+            self.calibrations[player_id]['y'][position] = []
+        self.calibrations[player_id]['x'][position].append(x)
+        self.calibrations[player_id]['y'][position].append(y)
+        print(f"Calibration for {position}: {self.calibrations[player_id]['x'][position]}")
+        print(self.calibrations)
+        if len(self.calibrations[player_id]['x'][position]) >= 5 and len(self.calibrations[player_id]['y'][position]) >= 5:
+            await self.next_calibration_position(position, player_id)
 
 
     async def fire(self, player_id):
@@ -111,11 +139,11 @@ class Room:
         if not self.computer_client:
             print("No computer client connected to update cursor position.")
             return
-        diff_x = angle_difference(x, self.calibrations_final['x']["center"])
-        diff_y = angle_difference(y, self.calibrations_final['y']["center"])
+        diff_x = angle_difference(x, self.calibrations_final[player_id]['x']["center"])
+        diff_y = angle_difference(y, self.calibrations_final[player_id]['y']["center"])
         print(f"Diff X: {diff_x}, Diff Y: {diff_y}, X: {x} Y: {y}")
-        shift_x = -math.copysign(math.fabs(math.tan(diff_x * math.pi / 180) / math.tan(self.calibrations_final['x']["range"] * math.pi / 180)) , diff_x)* 50 + 50
-        shift_y = math.copysign(math.fabs(math.tan(diff_y * math.pi / 180) / math.tan(self.calibrations_final['y']["range"] * math.pi / 180)) , diff_y)* 50 + 50
+        shift_x = -math.copysign(math.fabs(math.tan(diff_x * math.pi / 180) / math.tan(self.calibrations_final[player_id]['x']["range"] * math.pi / 180)) , diff_x)* 50 + 50
+        shift_y = math.copysign(math.fabs(math.tan(diff_y * math.pi / 180) / math.tan(self.calibrations_final[player_id]['y']["range"] * math.pi / 180)) , diff_y)* 50 + 50
         new_position_x = max(min(shift_x, 100), 0)
         new_position_y = max(min(shift_y, 100), 0)
         await self.computer_client.send_message(json.dumps({"type": "update", "player_id": player_id, "x": new_position_x, "y": new_position_y}))
@@ -123,9 +151,9 @@ class Room:
     async def start(self, websocket, client_type):
         
         if client_type == "mobile":
-            client = self.add_mobile_client(websocket)
+            client = await self.add_mobile_client(websocket)
         elif client_type == "computer":
-            client = self.set_computer_client(websocket)
+            client = await self.set_computer_client(websocket)
         else:
             print(f"Unknown client type: {client_type}")
             return
@@ -134,6 +162,10 @@ class Room:
                 print(f"Waiting for messages from {client_type} client.")
                 await client.update()
         except WebSocketDisconnect:
+            print("Client disconnected.")
+            print(f"Removing client: {client.id}")
+            print(f"Mobile counts: {len(self.mobile_clients)}")
+            
             self.remove_client(client)
         except Exception as e:
             print(f"Error in client connection: {e}")
@@ -163,7 +195,7 @@ class Client:
         await self.websocket.send_text(json.dumps({"type": "request_calibration", "position": new_calibration}))
     
 class MobileClient(Client):
-    def __init__(self, websocket, room, id=None):
+    def __init__(self, websocket, id, room):
         if id is None:
             id = f"mobile_{len(room.mobile_clients) + 1}"
         super().__init__(websocket,id, "mobile", room)
@@ -173,8 +205,8 @@ class MobileClient(Client):
         position = data['position']
         x = float(data['x'])
         y = float(data['y'])
-        await self.room.add_calibration_data(position, x, y)
-        print(f"Calibration for {position}: {self.room.calibrations['x'][position]}")
+        await self.room.add_calibration_data(position, x, y, self.id)
+        print(f"Calibration for {position}: {self.room.calibrations[self.id]['x'][position]}")
 
     async def handle_fire_event(self):
         await self.room.fire(self.id)
@@ -197,17 +229,23 @@ class MobileClient(Client):
             await self.handle_update_event(data)
 
 class ComputerClient(Client):
-    def __init__(self, websocket, id="computer"):
-        super().__init__(websocket, id, "computer", None)
+    def __init__(self, websocket, id,room):
+        super().__init__(websocket, id, "computer", room)
     
     async def handle_fire_event(self, player_id):
         await self.send_message(json.dumps({"type": "fire", "player_id": player_id}))
     
-    async def handle_update_event(self, data):
-        await self.send_message(json.dumps({"type": "update", "player_id": data['player_id'], "x": data['x'], "y": data['y']}))
+    async def handle_update_event(self, data, player_id):
+        await self.send_message(json.dumps({"type": "update", "player_id": player_id, "x": data['x'], "y": data['y']}))
 
-    async def on_calibration_complete(self):
-        await self.send_message(json.dumps({"type": "calibration_complete"}))
-        
+    async def on_calibration_complete(self, player_id):
+        await self.send_message(json.dumps({"type": "calibration_complete", "player_id": player_id}))
+
     async def fire(self, player_id):
         await self.send_message(json.dumps({"type": "fire", "player_id": player_id}))
+        
+    async def new_player_connected(self, player_id):
+        await self.send_message(json.dumps({"type": "new_player", "player_id": player_id}))
+        
+    async def request_calibration(self, new_calibration, player_id):
+        await self.send_message(json.dumps({"type": "request_calibration", "position": new_calibration, "player_id": player_id}))
--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from fastapi import FastAPI, Request, WebSocket
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import uvicorn
-import json
-import math
-import asyncio
-from fastapi.websockets import WebSocketDisconnect
 from Room import Room
 app = FastAPI()
 
@@ -24,30 +20,9 @@ async def get(request: Request):
 async def get_mobile(request: Request):
     return templates.TemplateResponse("mobile.html", {"request": request})
 
-mobile_connections = None
-computer_connections = None
-calibration_status = None
-
-next_calibration = {
-    "center":"left",
-    "left":"right",
-    "right":None
-}
-
-calibrations_final = {
-    "x": {},
-    "y": {},
-}
-calibrations = {
-    "x": {},
-    "y": {}
-}
-
-
 room = Room(1)
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    global mobile_connections, computer_connections, calibrations, calibration_status, next_calibration,calibrations_final
     # get the string parameters from the websocket url parameters
     client_type = websocket.query_params.get("client")
     print(f"Client type: {client_type}")
